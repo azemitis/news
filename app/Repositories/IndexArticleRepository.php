bc9274f4925e1993694d0c1240c8f7875abf634d
azemitis <7-days@inbox.lv>
2023-05-19 16:40:43 +0300

    Refactoring methods ArticleRepository

---
+++ app/Repositories/IndexArticleRepository.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace App\Repositories;
+
+use App\Models\Article;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class IndexArticleRepository
+{
+    private Client $httpClient;
+
+    public function __construct(Client $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    public function getAllArticles(): array
+    {
+        // Fetch articles from the external API
+        $url = 'https://jsonplaceholder.typicode.com/posts';
+        $response = $this->httpClient->get($url);
+        $body = $response->getBody()->getContents();
+        $data = json_decode($body, true);
+
+        // Transform API data into Article objects
+        $articles = [];
+        foreach ($data as $article) {
+            // Create an Article object based on the API data
+            $id = $article['id'];
+            $userId = $article['userId'];
+            $title = $article['title'];
+            $body = $article['body'];
+
+            $articleObject = new Article($userId, $id, $title, $body, $userId);
+
+            // Add the Article object to the array
+            $articles[] = $articleObject;
+        }
+
+        return $articles;
+    }
+}
